d67b2ed77e749ccd1cb4d0f61450c441ea255400
landon burnier <landonburnier@gmail.com>
2016-08-11 15:48:32 -0400

    avg and greaterthan

--- app.js
+++ app.js
@@ -1,25 +1,62 @@
-console.log('sum');
+// console.log('sum');
 
 
 
-function add (items) {
-let sum = 0;
+// function add (items) {
+// let sum = 0;
 
-for (let i = 0; i < items.length; i++) {
-sum = sum + items[i].number1 + items[i].number2;
-}
+// for (let i = 0; i < items.length; i++) {
+// sum = sum + items[i].number1 + items[i].number2;
+// }
 
-return sum;
-}
+// return sum;
+// }
 
-let input = [];
-input.push({
-    number1: 357,
-    number2: 289,
-});
+// let input = [];
+// input.push({
+//     number1: 357,
+//     number2: 289,
+// });
 
 
-let answer = add(input);
-console.log(answer);
+// let answer = add(input);
+// console.log(answer);
+
+
+
+
+// console.log('avg');    
+
+
+// function avg (items) {
+// let end = 0;
+
+// for (let i = 0; i < items.length; i++) {
+//     end = end + items[i] / items.length; // items[i] means all the items in the array
+   
+// }
+// return end;
+// }
+
+
+// let numbers = [6, 5, 13];
+
+// let answer2 = avg(numbers);
+// console.log(answer2);
+
+
+
+// console.log('greaterThan');
+
+// function greaterthan (first, second) {
+//     if (first < second) {
+//         return true;
+//     }else{
+//         return false;
+//     }        
+// }
+
+// let answer = greaterthan(4, 5);
+// console.log(answer);
 
 
