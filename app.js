fc12f43952a93714e68f2ad1d14f17217b48f878
landon burnier <landonburnier@gmail.com>
2016-08-11 19:35:32 -0400

    contains vowel

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // console.log('sum');
 
-
-
 // function add (items) {
 // let sum = 0;
 
@@ -25,6 +23,10 @@
 
 
 
+
+
+
+
 // console.log('avg');    
 
 
@@ -42,7 +44,13 @@
 // let numbers = [6, 5, 13];
 
 // let answer2 = avg(numbers);
-// console.log(answer2);
+// console.log(answer);
+
+
+
+
+
+
 
 
 
@@ -60,3 +68,51 @@
 // console.log(answer);
 
 
+
+
+
+
+
+
+
+
+
+// console.log('second largest');
+
+
+// function secondlargest (items){
+//     let biggest = 0;
+//     for (let i = 0; i < items.length; i++) {         // if first item in the array is bigger than 
+//          if (items[0] > items[1]){                  // the next then move on. If not then start with the next number
+// //how to refer to specific items in an array 
+//          }                    
+//     }
+// }
+
+// let numbers = [77, 5, 9, 18]; 
+
+
+
+
+
+
+
+
+console.log('contains vowel');
+
+function containsvowel (items) {
+
+for(let i = 0; i < items.length; i++) {
+    if (items[i].match(/[a, e, i, o, u]/i)) {    // this means if all items in an 
+        return true;                             // array has an a, e, i, o, u, then display true 
+}else{
+    return false;
+} 
+}
+}
+
+
+let word = ['Landon'];
+
+let answer = containsvowel(word);
+console.log(answer);
