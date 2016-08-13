350f62ab2b7017074fe8bc652bbf39fc2fe7ee45
landon burnier <landonburnier@gmail.com>
2016-08-13 15:36:23 -0400

    stuff

--- app.js
+++ app.js
@@ -171,105 +171,152 @@
 
 
 
-console.log('divisors')
-
-let numb3r = 60;
 
 
-        function divisors1 (items) {
-            let x = 0;
-            // let y = [];
-                if(items % 2 === 0) {
-                    x = x + items / 2;
-                    // y.push(x)[0];
-                }
+// console.log('divisors')
+
+// let numb3r = 60;
+
+
+//         function divisors1 (items) {
+//             let x = 0;
+//             // let y = [];
+//                 if(items % 2 === 0) {
+//                     x = x + items / 2;
+//                     // y.push(x)[0];
+//                 }
                 
-            return x;
-        }
-                    function divisors2 (items) {
-                    let x1 = 0;
-                    // let y = [];
-                        if(items % 3 === 0) {
-                            x1 = x1 + items / 3;
-                            // y.push(x);
-                        }
-                    return x1;
-                    }
+//             return x;
+//         }
+//                     function divisors2 (items) {
+//                     let x1 = 0;
+//                     // let y = [];
+//                         if(items % 3 === 0) {
+//                             x1 = x1 + items / 3;
+//                             // y.push(x);
+//                         }
+//                     return x1;
+//                     }
 
 
-                            function divisors3 (items) {
-                            let x2 = 0;
-                            // let y = [];
-                                if(items % 4 === 0) {
-                                    x2 = x2 + items / 4;
-                                    // y.push(x);
-                                }
-                            return x2;
-                            }
+//                             function divisors3 (items) {
+//                             let x2 = 0;
+//                             // let y = [];
+//                                 if(items % 4 === 0) {
+//                                     x2 = x2 + items / 4;
+//                                     // y.push(x);
+//                                 }
+//                             return x2;
+//                             }
                 
-                                    function divisors4 (items) {
-                                    let x3 = 0;
-                                    // let y = [];
-                                        if(items % 5 === 0) {
-                                            x3 = x3 + items / 5;
-                                            // y.push(x);
-                                        }
-                                    return x3;
-                                    }
-                                            function divisors5 (items) {
-                                            let x4 = 0;
-                                            // let y = [];
-                                                if(items % 6 === 0) {
-                                                    x4 = x4 + items / 6;
-                                                    // y.push(x);
-                                                }
-                                            return x4;
-                                            }
-                                                    function divisors6 (items) {
-                                                    let x5 = 0;
-                                                    // let y = [];
-                                                        if(items % 7 === 0) {
-                                                            x5 = x5 + items / 7;
-                                                            // y.push(x);
-                                                        }
-                                                    return x5;
-                                                    }
-                                                            function divisors7 (items) {
-                                                            let x6 = 0;
-                                                            // let y = [];
-                                                                if(items % 8 === 0) {
-                                                                    x6 = x6 + items / 8;
-                                                                    // y.push(x);
-                                                                }
-                                                            return x6;
-                                                            }
-                                                                    function divisors8 (items) {
-                                                                    let x7 = 0;
-                                                                    // let y = [];
-                                                                        if(items % 9 === 0) {
-                                                                            x7 = x7 + items / 9;
-                                                                            // y.push(x);
-                                                                        }
-                                                                    return x7;
-                                                                    }
+//                                     function divisors4 (items) {
+//                                     let x3 = 0;
+//                                     // let y = [];
+//                                         if(items % 5 === 0) {
+//                                             x3 = x3 + items / 5;
+//                                             // y.push(x);
+//                                         }
+//                                     return x3;
+//                                     }
+//                                             function divisors5 (items) {
+//                                             let x4 = 0;
+//                                             // let y = [];
+//                                                 if(items % 6 === 0) {
+//                                                     x4 = x4 + items / 6;
+//                                                     // y.push(x);
+//                                                 }
+//                                             return x4;
+//                                             }
+//                                                     function divisors6 (items) {
+//                                                     let x5 = 0;
+//                                                     // let y = [];
+//                                                         if(items % 7 === 0) {
+//                                                             x5 = x5 + items / 7;
+//                                                             // y.push(x);
+//                                                         }
+//                                                     return x5;
+//                                                     }
+//                                                             function divisors7 (items) {
+//                                                             let x6 = 0;
+//                                                             // let y = [];
+//                                                                 if(items % 8 === 0) {
+//                                                                     x6 = x6 + items / 8;
+//                                                                     // y.push(x);
+//                                                                 }
+//                                                             return x6;
+//                                                             }
+//                                                                     function divisors8 (items) {
+//                                                                     let x7 = 0;
+//                                                                     // let y = [];
+//                                                                         if(items % 9 === 0) {
+//                                                                             x7 = x7 + items / 9;
+//                                                                             // y.push(x);
+//                                                                         }
+//                                                                     return x7;
+//                                                                     }
 
-let answer1 = divisors1(numb3r);
-let answer2 = divisors2(numb3r);
-let answer3 = divisors3(numb3r);
-let answer4 = divisors4(numb3r);
-let answer5 = divisors5(numb3r);
-let answer6 = divisors6(numb3r);
-let answer7 = divisors7(numb3r);
-let answer8 = divisors8(numb3r);
+// let answer1 = divisors1(numb3r);
+// let answer2 = divisors2(numb3r);
+// let answer3 = divisors3(numb3r);
+// let answer4 = divisors4(numb3r);
+// let answer5 = divisors5(numb3r);
+// let answer6 = divisors6(numb3r);
+// let answer7 = divisors7(numb3r);
+// let answer8 = divisors8(numb3r);
 
-let array = [];
-array.push(answer1);
-array.push(answer2);
-array.push(answer3);
-array.push(answer4);
-array.push(answer5);
-array.push(answer6);
-array.push(answer7);
-array.push(answer8);
-// math 
-console.log(array); 
+// let array = [];
+// array.push(answer1);
+// array.push(answer2);
+// array.push(answer3);
+// array.push(answer4);
+// array.push(answer5);
+// array.push(answer6);
+// array.push(answer7);
+// array.push(answer8);
+// // math 
+// console.log(array); 
+
+
+
+
+console.log('weave'); 
+
+
+
+function replacer (item) {
+let wordtouse = word.split(" ");
+let position = 4; 
+    for (let i = 0; i < wordtouse.length; i++) {
+        item[i] = item[i] + position;
+    }
+  return wordtouse;  
+}
+
+
+
+
+
+let answer = replacer('spaghetti');
+console.log(answer);
+
+
+// console.log('longest word');
+
+// function find (sentence) {
+
+// let sentencesplit = sentence.split(" ");   //turning it into a number and then that number into a word.
+// let longestword = 0; 
+// let word = "";
+
+// for (let i = 0; i < sentencesplit.length; i++) {
+//     // if this is the longest weve seen so far, save it
+//     if (sentencesplit[i].length > longestword) {
+//         longestword = sentencesplit[i].length; 
+//         word = sentencesplit[i]; 
+//     }
+// }
+// return word;
+// }
+ 
+// let answer = find('What is the longest word?');
+// console.log(answer);
